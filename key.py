e8066062d5e490ace9e1c3e2ee05b37956e97f43
hilmyyy <115638253+hilmy1509@users.noreply.github.com>
2024-11-22 00:18:48 +0700

    Update key.py

--- key.py
+++ key.py
@@ -1,14 +1,11 @@
 from Crypto.PublicKey import RSA
 
-# Membuat pasangan kunci RSA
 key = RSA.generate(2048)
 
-# Menyimpan kunci privat ke file
 private_key = key.export_key()
 with open("private.pem", "wb") as private_file:
     private_file.write(private_key)
 
-# Menyimpan kunci publik ke file
 public_key = key.publickey().export_key()
 with open("public.pem", "wb") as public_file:
     public_file.write(public_key)
